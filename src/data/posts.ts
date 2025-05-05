cf6cf7ca7d5e3012b0ddc57f55e0e5b54a3df21a
Minh Hien <minh.hien.0115@gmail.com>
2025-05-05 21:57:11 +0700

    BaiTap

--- src/data/posts.ts
+++
@@ -1,9 +0,0 @@
-export const posts = [
-    {
-      "userId": 1,
-      "id": 1,
-      "email": "nguyenminhhien07022018@gmail.com",
-      "password": "123456",
-      "name": "Nguyễn Minh Hiền",
-    },
-  ]
--- src/screens/Tab/index.ts
+++
@@ -1,9 +0,0 @@
-import { Menu, Video } from "iconsax-react-native";
-import BanBe from "./BanBe";
-import NhanTin from "./NhanTin";
-import ThongBao from "./ThongBao";
-import TrangChu from "./TrangChu";
-
-export {
-    BanBe, Menu, NhanTin, ThongBao, TrangChu, Video
-}
